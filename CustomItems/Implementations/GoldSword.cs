ad0fa0466319bd51da9569bd49fd9aa27bbcb93d
Intybyte <vaan.testwork@gmail.com>
2024-11-15 15:20:31 +0100

    Remove debugging

--- CustomItems/Implementations/GoldSword.cs
+++ CustomItems/Implementations/GoldSword.cs
@@ -9,14 +9,10 @@ namespace CustomItems.Implementations
 
         public override void TriggerEffect(Dictionary<BattleTurn, BattleStats> battleStats, BattleSystem battleSystem)
         {
-            Debug.Log("Start Gold");
             int gold = StatsManager.Instance.playerGold;
-            Debug.Log("Got Gold");
             int bonusAtk = gold / 10;
             if(bonusAtk == 0) return;
-            Debug.Log("Gold nope");
             battleSystem.EnqueueEffect(battleSystem.CO_DamageTrigger(HicGeneric.GetOppositeTurn(this.player), bonusAtk, DamageSource.ATTACK, this.sprite, this));
-            Debug.Log("Enqueued");
         }
 
         public void OnHit(Dictionary<BattleTurn, BattleStats> battleStats, BattleSystem battleSystem, EffectBase effectSource)
