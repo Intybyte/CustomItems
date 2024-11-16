8020936bdb757bdb1a61ff6c1b59ecc263060d1c
Intybyte <vaan.testwork@gmail.com>
2024-11-16 11:05:17 +0100

    Add methods to SpriteUtil

--- CustomItems/SpriteUtil.cs
+++ CustomItems/SpriteUtil.cs
@@ -8,6 +8,51 @@ namespace CustomItems
         public static readonly Color background = new Color(37f / 255f, 19f / 255f, 26f / 255f, 0);
 
         #region Colors
+
+        public static Sprite SetColor(string sprite, int add_r, int add_g, int add_b)
+        {
+            return SpriteRegistry.sprites[sprite].SetColor(add_r, add_g, add_b);
+        }
+
+        public static Sprite SetColor(this Sprite sprite, int set_r, int set_g, int set_b) {
+            var texture = sprite.texture;
+
+            float red = set_r / 255f;
+            float green = set_g / 255f;
+            float blue = set_b / 255f;
+
+            Texture2D newTexture = new Texture2D(texture.width, texture.height, TextureFormat.RGBA32, false)
+            {
+                filterMode = FilterMode.Point,
+                wrapMode = TextureWrapMode.Clamp
+            };
+
+            for (int y = 0; y < texture.height; y++)
+            {
+                for (int x = 0; x < texture.width; x++)
+                {
+
+                    Color pixel = texture.GetPixel(x, y);
+                    if (IsBackground(pixel))
+                    {
+                        newTexture.SetPixel(x, y, background);
+                        continue;
+                    }
+
+                    newTexture.SetPixel(x, y, new Color(red, green, blue));
+                }
+            }
+
+            newTexture.Apply();
+
+            return Sprite.Create(
+                newTexture,
+                sprite.textureRect,
+                new Vector2(0.5f, 0.5f),
+                sprite.pixelsPerUnit
+            );
+        }
+
         public static Sprite AddColor(string sprite, int add_r, int add_g, int add_b)
         {
             return SpriteRegistry.sprites[sprite].AddColor(add_r, add_g, add_b);
@@ -21,9 +66,11 @@ namespace CustomItems
             float green = add_g / 255f;
             float blue = add_b / 255f;
 
-            Texture2D newTexture = new Texture2D(texture.width, texture.height, TextureFormat.RGBA32, false);
-            newTexture.filterMode = FilterMode.Point;
-            newTexture.wrapMode = TextureWrapMode.Clamp;
+            Texture2D newTexture = new Texture2D(texture.width, texture.height, TextureFormat.RGBA32, false)
+            {
+                filterMode = FilterMode.Point,
+                wrapMode = TextureWrapMode.Clamp
+            };
 
             for (int y = 0; y < texture.height; y++)
             {
